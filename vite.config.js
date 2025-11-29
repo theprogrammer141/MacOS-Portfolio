c8d4de8eabc8f23e6b2f05f0c4a54c8cf15c8b59
theprogrammer141 <abdullah.k9913@gmail.com>
2025-11-29 21:50:39 +0500

    first commit

---
+++ vite.config.js
@@ -0,0 +1,27 @@
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { dirname, resolve } from "path";
+import { fileURLToPath } from "url";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "#components": resolve(
+        dirname(fileURLToPath(import.meta.url)),
+        "src/components"
+      ),
+      "#constants": resolve(
+        dirname(fileURLToPath(import.meta.url)),
+        "src/constants"
+      ),
+      "#store": resolve(dirname(fileURLToPath(import.meta.url)), "src/store"),
+      "#hoc": resolve(dirname(fileURLToPath(import.meta.url)), "src/hoc"),
+      "#windows": resolve(
+        dirname(fileURLToPath(import.meta.url)),
+        "src/windows"
+      ),
+    },
+  },
+});
